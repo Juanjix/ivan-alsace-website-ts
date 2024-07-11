359b850ad2b2ab7b7298fa8cfd7d51033b531a5d
Juan José Peralta <juanjose.peralta92@gmail.com>
2024-07-11 00:21:30 -0300

    metrics / wip

---
+++ public/icons/check/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Check() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32">
+      <path
+        d="M10 16L14.2426 20.2427L22.7279 11.7574"
+        stroke="#02FE57"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"></path>
+      <circle cx="16" cy="16" r="15" stroke="#02FE57" stroke-width="2"></circle>
+    </svg>
+  );
+}
+
+export default Check;
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
+import Check from "../../public/icons/check/index";
+
+// Components
+import { Button } from "./Button";
+
 const StyledPricing = styled.section`
   .pricing-cards-container {
     display: flex;
     flex-direction: row;
     justify-content: center;
-    gap: 30px;
+    gap: 10px;
+    margin-top: 40px;
 
     .pricing-card {
       max-width: 370px;
@@ -13,7 +19,31 @@ const StyledPricing = styled.section`
       height: auto;
       border: 1px solid grey;
       border-radius: 18px;
-      padding: 40px 18px;
+      padding: 40px 10px;
+
+      h3 {
+        margin-bottom: 22px;
+      }
+
+      ul {
+        list-style: none;
+        padding-left: 0;
+        li {
+          position: relative;
+          margin-bottom: 25px;
+          display: flex;
+          justify-content: center;
+
+          svg {
+            width: 20px;
+            height: 20px;
+          }
+        }
+      }
+
+      &:hover {
+        border: 1px solid #e0c68f;
+      }
     }
   }
 `;
@@ -30,23 +60,104 @@ const Pricing = () => {
         <div className="pricing-card">
           <h3>Started</h3>
           <ul>
-            <li>lista de prueba</li>
+            <li>
+              <Check />
+              The only sample pack you need to finally produce placement-worthy
+              beats
+            </li>
           </ul>
           <ul>
-            <li>lista de prueba</li>
+            <li>
+              <Check />
+              The only sample pack you need to finally produce placement-worthy
+              beats
+            </li>
           </ul>
           <ul>
-            <li>lista de prueba</li>
+            <li>
+              {" "}
+              <Check />
+              ​Destroy beat block once and for all by having infinite
+              inspiration at your fingertips
+            </li>
           </ul>
           <ul>
-            <li>lista de prueba</li>
+            <li>
+              {" "}
+              <Check />
+              Skyrocket your beat sales by producing the hottest beats on the
+              market
+            </li>
           </ul>
+          <Button texto={"Get the blue print now"} url={""} />
         </div>
         <div className="pricing-card">
-          <h3>Started</h3>
+          <h3>Profesional</h3>
+          <ul>
+            <li>
+              <Check />
+              The only sample pack you need to finally produce placement-worthy
+              beats
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Check />
+              The only sample pack you need to finally produce placement-worthy
+              beats
+            </li>
+          </ul>
+          <ul>
+            <li>
+              {" "}
+              <Check />
+              ​Destroy beat block once and for all by having infinite
+              inspiration at your fingertips
+            </li>
+          </ul>
+          <ul>
+            <li>
+              {" "}
+              <Check />
+              Skyrocket your beat sales by producing the hottest beats on the
+              market
+            </li>
+          </ul>
+          <Button texto={"Get the blue print now"} url={""} />
         </div>
         <div className="pricing-card">
-          <h3>Started</h3>
+          <h3>Team</h3>
+          <ul>
+            <li>
+              <Check />
+              The only sample pack you need to finally produce placement-worthy
+              beats
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Check />
+              The only sample pack you need to finally produce placement-worthy
+              beats
+            </li>
+          </ul>
+          <ul>
+            <li>
+              {" "}
+              <Check />
+              ​Destroy beat block once and for all by having infinite
+              inspiration at your fingertips
+            </li>
+          </ul>
+          <ul>
+            <li>
+              {" "}
+              <Check />
+              Skyrocket your beat sales by producing the hottest beats on the
+              market
+            </li>
+          </ul>
+          <Button texto={"Get the blue print now"} url={""} />
         </div>
       </div>
     </StyledPricing>
